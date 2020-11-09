834a2955616809e9bbae8bccbfb301e8989423df
xulei650 <409931518@qq.com>
2020-11-09 11:27:02 +0800

    22

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include <string>
 int main(int argc, char *argv[]) {
     if (1) {
         std::cout <<"111111111111112333333333333333333333333333333333333333333" <<std::endl;
     }
+
 
 
   std::string message = "Hello World";
