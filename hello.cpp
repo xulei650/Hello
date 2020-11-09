e74de6c7a49ae7e7b20565f0eed5a5a261c98690
xulei650 <409931518@qq.com>
2020-11-09 11:23:39 +0800

    11

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
     }
 
 
-    std::string message = "Hello World";
+  std::string message = "Hello World";
     std::cout << message << std::endl;
     return 0;
 }
